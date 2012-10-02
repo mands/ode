751a6a5ebc9ba934ae6f189390c29fa8c0adcff8
mands <mandeepg@gmail.com>
2012-10-02 19:01:08 +0100

    Working on using Cog to generate the VecMath thunking funcs

---
+++ res/utils/VecMath/VecMathCog.py
@@ -0,0 +1,181 @@
+import cog
+
+class MathLib:
+    """Enumeration of possible content types"""
+    (Intel, AMD, GNU) = range(3)
+
+# basic functions of type f->f
+# TODO - 'invsqrt', 'invcbrt',
+funcsFtoF = [ 'sin', 'cos', 'tan'                        # trig funs
+    , 'asin', 'acos', 'atan'
+    , 'sinh', 'cosh', 'tanh'
+    , 'asinh', 'acosh', 'atanh'
+    , 'exp', 'exp2', 'exp10', 'expm1'           # exp funcs
+    , 'log', 'log2', 'log10', 'logb', 'log1p'   # log funcs
+    , 'sqrt', 'cbrt', 'pow10'                   # power funcs
+    , 'erf', 'erfc'                             # err funcs
+]
+
+# TODO - not yet implemented
+funcsFFtoF =    [ 'atan2', 'pow', 'hypot' ]
+
+# TODO - not yet implemented
+funcsFtoFF =    []
+
+## Helper funcs
+def _getLLVMType(vecSize):
+    if vecSize == 1:
+        return "double"
+    else:
+        return "<{0} x double>".format(vecSize)
+
+def _getVecMathName(funcName, vecSize):
+    if vecSize == 1:
+        funcSuffix = "f64"
+    else:
+        funcSuffix = "v{0}f64".format(vecSize)
+    return "vecmath_{0}_{1}".format(funcName, funcSuffix)
+
+def getCallName(funcName, vecSize, mathLib):
+    if mathLib == MathLib.Intel:
+        if vecSize == 1:
+            return funcName
+        else:
+            return "__svml_{0}{1}".format(funcName, vecSize)
+
+    elif mathLib == MathLib.AMD:
+        pass
+
+    elif mathLib == MathLib.GNU:
+        return "__{0}_finite".format()
+    else:
+        pass
+
+def _checkVecSize(vecSize):
+    if vecSize == 1 or vecSize == 2 or vecSize == 4:
+        return True
+    else:
+        raise ValueError('vecSize not a valid number')
+
+def genProto(protoName, vecSize):
+    _checkVecSize(vecSize)
+    llvmType = _getLLVMType(vecSize)
+    cog.outl("declare {0} @{1}({0} %ins) nounwind readnone".format(llvmType, protoName))
+
+
+
+# a wrapper to call the correct genThunk based on the input params
+def genThunk(funcName, callName, inVecSize=1, callVecSize=1, inArgs=1, outArgs=1):
+    _checkVecSize(inVecSize)
+    _checkVecSize(callVecSize)
+    vecMathName = _getVecMathName(funcName, inVecSize)
+    if inVecSize == callVecSize:
+        if inArgs==1 and outArgs ==1:
+            _getThunk_0_1in_1out(vecMathName, callName, inVecSize)
+
+    elif inVecSize == 2 and callVecSize == 1:
+        if inArgs==1 and outArgs ==1:
+            _getThunk_2to1_1in_1out(vecMathName, callName)
+
+    elif inVecSize == 4 and callVecSize == 1:
+        if inArgs==1 and outArgs ==1:
+            _getThunk_4to1_1in_1out(vecMathName, callName)
+
+    elif inVecSize == 4 and callVecSize == 2:
+        if inArgs==1 and outArgs ==1:
+            _getThunk_4to2_1in_1out(vecMathName, callName)
+
+
+## All our basic "templates" for calling the vectorised math funcs go here
+def _getThunk_0_1in_1out(vecMathName, callName, vecSize):
+    """calls a packed func directly"""
+    llvmType = _getLLVMType(vecSize)
+    ## output the func definition
+    cog.outl("define {0} @{1}({0} %ins) nounwind alwaysinline readnone {{".format(llvmType, vecMathName))
+    cog.outl("entry:")
+    cog.outl("  %ret0 = tail call {0} @{1}({0} %ins) nounwind readnone".format(llvmType, callName))
+    cog.outl("  ret {0} %ret0".format(llvmType))
+    cog.outl("}")
+    cog.outl("")
+
+def _getThunk_2to1_1in_1out(vecMathName, callName):
+    """calls a packed func by unpacking a 2xdouble vector to singles"""
+    inType = _getLLVMType(2)
+    callType = _getLLVMType(1)
+    baseType = _getLLVMType(1)
+    ## output the func definition
+    cog.outl("define {0} @{1}({0} %ins) nounwind alwaysinline readnone {{".format(inType, vecMathName))
+    cog.outl("entry:")
+    # unpack the ins and call
+    cog.outl("  %in0 = extractelement {0} %ins, i32 0".format(inType))
+    cog.outl("  %ret0 = tail call {0} @{1}({0} %in0) nounwind readnone".format(callType, callName))
+    cog.outl("  %in1 = extractelement {0} %ins, i32 1".format(inType))
+    cog.outl("  %ret1 = tail call {0} @{1}({0} %in1) nounwind readnone".format(callType, callName))
+    # repack, and return
+    cog.outl("  %retvec0.0 = insertelement {0} undef, {1} ret0, i32 0".format(inType, baseType))
+    cog.outl("  %retvec0.1 = insertelement {0} retvec0.0, {1} ret1, i32 1".format(inType, baseType))
+    cog.outl("  ret {0} %retvec0.1".format(inType))
+    cog.outl("}")
+    cog.outl("")
+
+def _getThunk_4to1_1in_1out(vecMathName, callName):
+    """calls a packed func by unpacking a 2xdouble vector to singles"""
+    inType = _getLLVMType(4)
+    callType = _getLLVMType(1)
+    baseType = _getLLVMType(1)
+    ## output the func definition
+    cog.outl("define {0} @{1}({0} %ins) nounwind alwaysinline readnone {{".format(inType, vecMathName))
+    cog.outl("entry:")
+    # unpack the ins and call
+    cog.outl("  %in0 = extractelement {0} %ins, i32 0".format(inType))
+    cog.outl("  %ret0 = tail call {0} @{1}({0} %in0) nounwind readnone".format(callType, callName))
+    cog.outl("  %in1 = extractelement {0} %ins, i32 1".format(inType))
+    cog.outl("  %ret1 = tail call {0} @{1}({0} %in1) nounwind readnone".format(callType, callName))
+    cog.outl("  %in2 = extractelement {0} %ins, i32 2".format(inType))
+    cog.outl("  %ret2 = tail call {0} @{1}({0} %in2) nounwind readnone".format(callType, callName))
+    cog.outl("  %in3 = extractelement {0} %ins, i32 3".format(inType))
+    cog.outl("  %ret3 = tail call {0} @{1}({0} %in3) nounwind readnone".format(callType, callName))
+    # repack, and return
+    cog.outl("  %retvec0.0 = insertelement {0} undef, {1} ret0, i32 0".format(inType, baseType))
+    cog.outl("  %retvec0.1 = insertelement {0} retvec0.0, {1} ret1, i32 1".format(inType, baseType))
+    cog.outl("  %retvec0.2 = insertelement {0} retvec0.1, {1} ret2, i32 2".format(inType, baseType))
+    cog.outl("  %retvec0.3 = insertelement {0} retvec0.2, {1} ret3, i32 3".format(inType, baseType))
+    cog.outl("  ret {0} %retvec0.3".format(inType))
+    cog.outl("}")
+    cog.outl("")
+
+def _getThunk_4to2_1in_1out(vecMathName, callName):
+    """calls a packed func by unpacking a 2xdouble vector to singles"""
+    inType = _getLLVMType(4)
+    callType = _getLLVMType(2)
+    baseType = _getLLVMType(1)
+    ## output the func definition
+    cog.outl("define {0} @{1}({0} %ins) nounwind alwaysinline readnone {{".format(inType, vecMathName))
+    cog.outl("entry:")
+
+    # extract 2 elems into a tmp vector and call
+    cog.outl("  %in0 = extractelement {0} %ins, i32 0".format(inType))
+    cog.outl("  %in1 = extractelement {0} %ins, i32 1".format(inType))
+    cog.outl("  %invec0.0 = insertelement {0} undef, {1} in0, i32 0".format(callType, baseType))
+    cog.outl("  %invec0.1 = insertelement {0} invec0.0, {1} in1, i32 1".format(callType, baseType))
+    cog.outl("  %retvec0 = tail call {0} @{1}({0} %invec0.1) nounwind readnone".format(callType, callName))
+
+    # extract 2 elems into a tmp vector and call
+    cog.outl("  %in2 = extractelement {0} %ins, i32 0".format(inType))
+    cog.outl("  %in3 = extractelement {0} %ins, i32 1".format(inType))
+    cog.outl("  %invec1.0 = insertelement {0} undef, {1} in2, i32 2".format(callType, baseType))
+    cog.outl("  %invec1.1 = insertelement {0} invec1.0, {1} in3, i32 3".format(callType, baseType))
+    cog.outl("  %retvec1 = tail call {0} @{1}({0} %invec1.1) nounwind readnone".format(callType, callName))
+
+    # unpack all vals, repack, and return
+    cog.outl("  %ret0 = extractelement {0} %retvec0, i32 0".format(callType))
+    cog.outl("  %ret1 = extractelement {0} %retvec0, i32 1".format(callType))
+    cog.outl("  %ret2 = extractelement {0} %retvec1, i32 0".format(callType))
+    cog.outl("  %ret3 = extractelement {0} %retvec1, i32 1".format(callType))
+    cog.outl("  %retvec2.0 = insertelement {0} undef, {1} ret0, i32 0".format(inType, baseType))
+    cog.outl("  %retvec2.1 = insertelement {0} retvec0.0, {1} ret1, i32 1".format(inType, baseType))
+    cog.outl("  %retvec2.2 = insertelement {0} retvec0.1, {1} ret2, i32 2".format(inType, baseType))
+    cog.outl("  %retvec2.3 = insertelement {0} retvec0.2, {1} ret3, i32 3".format(inType, baseType))
+    cog.outl("  ret {0} %retvec2.3".format(inType))
+    cog.outl("}")
+    cog.outl("")
---
+++ res/utils/VecMath/vecmath_intel_cog.ll
@@ -0,0 +1,58 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; VecMath Wrapper onto Intel SVML library
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; generate prototypes
+;;[[[cog 
+;;  import cog
+;;  import VecMathCog as C
+;;  # scalar prototypes
+;;  vecSize = 1
+;;  for f in C.funcsFtoF:
+;;    C.genProto(f, vecSize) 
+;;  # svml prototypes
+;;  vecSize = 2
+;;  for f in C.funcsFtoF:
+;;    protoName = C.getCallName(f, vecSize, C.MathLib.Intel)
+;;    C.genProto(protoName, vecSize)
+;;  # svml prototypes
+;;  vecSize = 4
+;;  for f in C.funcsFtoF:
+;;    protoName = C.getCallName(f, vecSize, C.MathLib.Intel)
+;;    C.genProto(protoName, vecSize)
+;;  
+;;]]]
+;;[[[end]]]
+
+
+;;[[[cog 
+;;  # scalar thunks
+;;  vecSize = 1
+;;  for f in C.funcsFtoF:
+;;    callName = C.getCallName(f, vecSize, C.MathLib.Intel)
+;;    C.genThunk(f, callName, vecSize, vecSize)
+;;]]]
+;;[[[end]]]
+
+
+;;[[[cog 
+;;  # svml thunks
+;;  vecSize = 2
+;;  for f in C.funcsFtoF:
+;;    callName = C.getCallName(f, vecSize, C.MathLib.Intel)
+;;    C.genThunk(f, callName, vecSize, vecSize)
+;;]]]
+;;[[[end]]]
+
+;;[[[cog 
+;;  # svml thunks
+;;  vecSize = 4
+;;  for f in C.funcsFtoF:
+;;    callName = C.getCallName(f, vecSize, C.MathLib.Intel)
+;;    C.genThunk(f, callName, vecSize, vecSize)
+;;]]]
+;;[[[end]]]
+;;  
+
+
+
